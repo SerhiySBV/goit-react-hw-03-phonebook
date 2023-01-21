36357758956baccc8cfab94ac8be7a6daa7298a1
SerhiySBV <serhiybelyavskii@gmail.com>
2023-01-21 19:08:39 +0200

    Added local storige

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,6 +47,19 @@ class App extends Component {
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsContacts = JSON.parse(contacts);
+    if (parsContacts) {
+      this.setState({ contacts: parsContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
